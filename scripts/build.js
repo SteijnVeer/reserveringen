6902293735d9eac93c8411bec4ab2d4402451aa9
Steijn <steijnveer2006@outlook.com>
2026-02-15 01:51:35 +0100

    fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,6 @@ import metadata from '../commons/metadata.json' with { type: 'json' };
 rmSync('out', { recursive: true, force: true });
 
 cpSync('public', 'out', { recursive: true });
-copyFileSync(`pages/404.html`, `out/404.html`);
 
 for (const [page, path] of Object.entries(metadata.paths)) {
   execSync(`cd pages/${page} && tsc -b && vite build`, { stdio: 'inherit' });
